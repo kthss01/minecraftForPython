efcd86d594850f5b64b95f076edf0c6cbebf6713
kthss01 <kthss01@naver.com>
2020-08-21 23:27:20 +0900

    Make Rainbow

---
+++ venv/MakeRainbow.py
@@ -0,0 +1,16 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+from math import *
+
+colors = [14, 1, 4, 5, 3, 11, 10]
+
+mc = minecraft.Minecraft.create()
+height = 60
+
+px, py, pz = mc.player.getPos()
+
+mc.setBlocks(-64 + px, 0 + py, 0 + pz, 64, height + len(colors), 0, 0)
+for x in range(0, 128):
+    for colourindex in range(0, len(colors)):
+        y = sin((x / 128.0) * pi) * height + colourindex
+        mc.setBlock(x - 64 + px, y + py, 0 + pz, block.WOOL.id, colors[len(colors) - 1 - colourindex])
